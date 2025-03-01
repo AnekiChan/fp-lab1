70e0c3316ff740e57ea9a32799da8ef63394073d
AnekiChan <lovepolina2004@gmail.com>
2025-03-01 22:15:40 +0300

    upd

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
 // 1.1
 const filterEvenNumbers = (numbers) => numbers.filter(num => num % 2 === 0);
-
 // 1.2
 const squareNumbers = (numbers) => numbers.map(num => num * num);
-
 // 1.3
 const filterByProperty = (objects, property) => objects.filter(obj => obj.hasOwnProperty(property));
-
 // 1.4
 const sumNumbers = (numbers) => numbers.reduce((acc, num) => acc + num, 0);
-
 // 2
 const applyFunctionToArray = (func, array) => array.map(func);
-
 // 3.1
-const numbers = [1, 4, 3, 8, 11, 16];
+const numbers = [23, 32, 2, 55, 3, 2];
 const evenNumbers = filterEvenNumbers(numbers);
 const squaredEvenNumbers = squareNumbers(evenNumbers);
 const sumOfSquares = sumNumbers(squaredEvenNumbers);
 console.log("Сумма квадратов четных чисел:", sumOfSquares);
 
-
 // 3.2
 const objects = [
-    { value: 12 },
-    { value: 32 },
-    { value: 45 },
-    { value: 66 }
+    { value: -1 },
+    { value: 544 },
+    { value: 23 },
+    { value: 9 }
 ];
-const threshold = 15;
+const threshold = 4;
 const filteredObjects = filterByProperty(objects, 'value');
 const valuesAboveThreshold = filteredObjects.filter(obj => obj.value > threshold).map(obj => obj.value);
 const average = valuesAboveThreshold.reduce((acc, val) => acc + val, 0) / valuesAboveThreshold.length;
